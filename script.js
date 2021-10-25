651ff71f3cc8b3736d0e5a1db2905617cf96b305
MA15 <holladay15@yahoo.com>
2021-10-24 21:55:59 -0400

    second commit

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // var weatherApiKey = "7ed1d607ce80dbd08a78034b7e12147e";
 
+https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key} <==== EXAMPLE Figure out get lat and lon? also exclude
+
 var button = document.querySelector('.button')
 var inputVal = document.querySelector('.inputVal')
 var name = document.querySelector('.name');
